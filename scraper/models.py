ca7f2186ec5d701a8e2086769133e946f00659e9
devights <devights@gmail.com>
2013-01-15 20:05:19 -0800

    fleshing out models

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Incident(models.Model):
+    incident_id = models.AutoField(primary_key=True)
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField(null=True)
+    location_text = models.CharField(max_length=255)
+    type_id = models.ForeignKey('IncidentType')
+    level = models.IntegerField()
+
+class IncidentType(models.Model):
+    incident_type_id = models.AutoField(primary_key=True)
+    type_name = models.CharField(max_length=255)
