58ff896998bc803ab23ef0ea547d0f81a8e58008
Stephen De Vight <devights@gmail.com>
2015-07-24 11:42:39 -0700

    adding app vars context processor

---
+++ app/context_processors.py
@@ -0,0 +1,4 @@
+def app_vars(request):
+    return {
+        'site_name': "Seattle Fire Tracker"
+    }
--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 def index(request):
 
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return render_to_response('index.html', context_instance=RequestContext(request))
